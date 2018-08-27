2e09a97b94b76f544598f70276f9c6e97a614ab2
caiqing0204 <caiqing0204@163.com>
2018-08-28 01:24:40 +0800

    [update setting file]

--- dev_task/settings.py
+++ dev_task/settings.py
@@ -90,14 +90,6 @@ WSGI_APPLICATION = 'dev_task.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {}
 if config.get('db', 'engine') == 'mysql':
     DB_HOST = config.get('db', 'host')
@@ -160,7 +152,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
