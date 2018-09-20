058b2503b2c5c6fdc6bf321ba8626fc29455dde5
caiqing0204 <caiqing0204@163.com>
2018-09-20 18:32:50 +0800

    [修复 未登陆状态无法访问url]

--- dev_task/urls.py
+++ dev_task/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', views.LoginView.as_view(), name="login"),
     url(r'^task/', include(taskurls)),
     url(r'^login/$', views.LoginView.as_view(), name="login"),
     url(r'^logout/$', views.LogoutView.as_view(), name="logout"),
-    url(r'^$', login_required(TemplateView.as_view(template_name="index.html")))
 ]
 
